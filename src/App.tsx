b5fcc4a568a735281d3beea35b85971d59907320
ejen019 <ndayakeessowe@gmail.com>
2026-05-05 17:30:24 +0100

    Fix : correction des noms des fichiers et routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PostDetail from "./pages/PostDetail";
+import PostList from "./pages/PostList";
+import UserList from "./pages/UserList";
+import UserProfile from "./pages/UserProfile";
+import Login from "./pages/Login";
 
 //composants temporaires (a l'interieur de App.tsx pour commencer)
-function Accueil(){
-  return <h1>Accueil</h1>
-}
 
 function App(){
   return (
     <BrowserRouter>
     <Routes>
-        <Route path="/" element={<Accueil/>}/>
-        
+        <Route path="/" element={<PostList/>}></Route>
+        <Route path="/posts/:id" element={<PostDetail/>}/>
+        <Route path="/users" element={< UserList/>}/>
+        <Route path="/users/:id" element={<UserProfile/>}/>
+        <Route path="/login" element={<Login/>}/>
     </Routes>
     </BrowserRouter>
   )
