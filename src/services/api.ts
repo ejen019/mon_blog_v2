8597376a22587d501cb5fd210e90f728c2be911b
ejen019 <ndayakeessowe@gmail.com>
2026-05-07 13:00:52 +0100

    Fix : import des types Posts et Comments pour corriger les autres fonctions malecrites

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,6 +6,8 @@
 // getUserPosts(userId)
 
 import type {Post} from '../types'
+import type { Comment } from '../types'
+import type { User } from '../types'
 
 const URL = 'https://jsonplaceholder.typicode.com'
 
@@ -15,33 +17,33 @@ export async function getPosts(): Promise<Post[]> {
     return data
 }
 
-export async function getPost(id: number): Promise<Post[]> {
+export async function getPost(id: number): Promise<Post> {
     const response = await fetch(`${URL}/posts/${id}`)
     const data = await response.json()
     return data
 } 
 
-export async function getPostComments(id: number): Promise<Post[]> {
+export async function getPostComments(id: number): Promise<Comment[]> {
     const response = await fetch(`${URL}/posts/${id}/comments`)
     const data = await response.json()
     return data
 }
 
-export async function getUsers(): Promise<Post> {
+export async function getUsers(): Promise<User[]> {
   const response = await fetch(`${URL}/users`)
   const data = await response.json()
   return data
 }
 
 
-export async function getUser(id: number): Promise<Post> {
-  const response = await fetch(`${URL}/posts/${id}`)
+export async function getUser(id: number): Promise<User> {
+  const response = await fetch(`${URL}/users/${id}`)
   const data = await response.json()
   return data
 }
 
-export async function getUserPosts(userId: number): Promise<Post> {
-  const response = await fetch(`${URL}/posts/${userId}`)
+export async function getUserPosts(userId: number): Promise<Post[]> {
+  const response = await fetch(`${URL}/users/${userId}/posts`)
   const data = await response.json()
   return data
 }
